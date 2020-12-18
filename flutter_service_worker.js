0be08490816517580b621034f901e56e9626eeb9
newdividexd <75103080+newdividexd@users.noreply.github.com>
2020-12-18 04:17:32 -0300

    Web 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "c310aea22818a1e0d4bf201c49e85374",
-"/": "c310aea22818a1e0d4bf201c49e85374",
-"main.dart.js": "ec2901aedd9861707dbb9baf4f08c051",
+"index.html": "a40a14a08329686a353ffff4ba3bc5b5",
+"/": "a40a14a08329686a353ffff4ba3bc5b5",
+"main.dart.js": "b29cc7029542a18ef030fca8f83555b5",
 "manifest.json": "a9f4eef945714c1289e37c706ad7f664",
 "version.json": "63ebd510914d992237c4882992969335"
 };
